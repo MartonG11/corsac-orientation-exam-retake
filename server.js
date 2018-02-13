69a35265581e21cddb905f68336f4896adcb395a
MartonG11 <gabormarton91@gmail.com>
2018-02-13 11:33:27 +0100

    GET users endpoint added

--- server.js
+++ server.js
@@ -18,7 +18,36 @@ const conn = mysql.createConnection({
   password: '19910616',
   database: 'reporter',
 });
+conn.connect((err) => {
+    if (err) {
+      console.log('Connection error.', err);
+      throw err;
+    }
+    console.log('Connected!');
+  });
 
+
+// FRONTEND ENDPOINT
+app.get('/', (req, res) => {
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'page/home.html'));
+  });
+
+// GET USERS
+app.get('/users', (req, res) => {
+    conn.query('SELECT * FROM users', (err, rows) => {
+      if (err) {
+        res.status(500);
+        res.json({
+          message: 'Problem to get the Users',
+        });
+        throw err;
+      }
+  
+      res.json(rows);
+    });
+  });
+  
 app.listen(PORT, () => {
     console.log(`App is up and running on port ${PORT}`);
   });
